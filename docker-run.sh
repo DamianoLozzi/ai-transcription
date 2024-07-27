0fac0a59984d0551a6ec28460c82c28e967e36e1
Damiano Lozzi <damianolozzi1989@gmail.com>
2024-07-27 16:42:55 +0200

    Refactor docker-run.sh script to remove unnecessary --progress flag

--- docker-run.sh
+++ docker-run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-docker build --progress=plain -t ai-assistant . && \
+docker build -t ai-assistant . && \
 docker run -it ai-assistant ||\
 echo -e "\e[31mFailed to build and run the container\e[0m"
